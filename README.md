b2b3c5d16ee5b18fbf59daf2ac5c73cfb8a9e6d9
Devon Zhen <zhen.devon@gmail.com>
2021-04-16 14:59:29 -0400

    Store

--- README.md
+++ README.md
@@ -25,3 +25,26 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+## Website code setup in Visual Studio Code
+   Go to Top menu
+   1. File/Open Folder...
+      Select the RegistrationWeb which you have cloned from the GITHUB.
+   2. Terminal/New Terminal
+      Execute the following installation in New Terminal
+      a) npm install     
+      b) npm i-g @angular/cli   (sudo)
+      c) npm i -g typescript
+      d) npm i bootstrap 
+         copy @import '~bootstrap/dist/css/bootstrap.min.css'; to styles.css
+      e) npm i --save font-awesome angular-font-awesome
+         copy @import "~font-awesome/css/font-awesome.css"; to styles.css
+      f) npm i @angular/cdk @angular/material @angular/animations hammers
+         copy @import "~@angular/material/prebuilt-themes/indigo-pink.css"; to style.css
+      g) npm i @material-extended/mde    
+	   h) npm ingx-mask  
+      i) npm i ngx-show-hide-password 
+      j) npm i @fortawesome/angular-fontawesome @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons
+      k) matIcon:
+         copy @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatTabsModule} from '@angular/material/tabs'; 
+import {MatDividerModule} from '@angular/material/divider'; 
+import {MatSliderModule} from '@angular/material/slider'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatPaginatorModule} from '@angular/material/paginator'; 
+import {MatRadioModule} from '@angular/material/radio'; 
+
+//Modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,BrowserAnimationsModule,MatFormFieldModule,MatInputModule,
+    MatCardModule,MatPaginatorModule,MatSliderModule,MatRadioModule,MatDividerModule,MatTabsModule,
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
