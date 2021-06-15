961a276ca06afada026a4308739ff34cbdef4a2b
Devon Zhen <dz89@nji.edu>
2021-06-15 19:59:47 -0400

    Update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,15 +18,17 @@ export class HomeComponent implements OnInit {
   customerForm: FormGroup;
   orderForm: FormGroup;
 
-  constructor(
-    private api: DataService,
-    public dialog: MatDialog,
-    private formBuilder: FormBuilder
-  ) {}
+  constructor(private api: DataService,
+              public dialog: MatDialog,
+              private formBuilder: FormBuilder) 
+  {
+    this.buildCustomerForm();
+  }
 
   ngOnInit(): void {
-    this.buildCustomerForm();
+
     this.customerData();
+
   }
 
   onSubmit() {
@@ -38,11 +40,15 @@ export class HomeComponent implements OnInit {
   //Display JSON Data
   customerData() {
     this.api.getCustomerData().subscribe((data) => {
-      console.log("FULL CUSTOMER DATA: " + JSON.stringify(data));
-      for (var d in data) {
-        console.log("Loop: " + JSON.stringify(data[d]));
-        this.loadCustomerForm(data[d]);
-      }
+      console.log("FULL CUSTOMER DATA: " + JSON.stringify(data));   
+      data.forEach(customer => {
+        //console.log("Loop order: " + JSON.stringify(customer.orders));
+        customer.orders.forEach(order => {
+          //console.log("Loop order: " + JSON.stringify(customer.orders));
+          this.addOrder(order);
+        });
+      });
+
     });
   }
 
@@ -58,155 +64,62 @@ export class HomeComponent implements OnInit {
       street: [""],
       zip: [""],
       //Order Array
-      orders: this.buildOrderForm(),
+      orders: this.formBuilder.array([]), //this.buildOrderForm(),
     });
-  }
-
-  //Build Order Form
-  buildOrderForm() {
-    var orderFormArray = this.formBuilder.array([]);
-    orderFormArray.push(
-      this.formBuilder.group({
-        id: [""],
-        orderId: [""],
-        orderStatus: [""],
-        orderDate: [""],
-        storeId: [""],
-        customerId: [""],
-        orderItems: this.buildOrderItemsForm(),
-      })
-    );
-    return orderFormArray;
-  }
-
-  //Build Order Items Form
-  buildOrderItemsForm() {
-    var orderItemsArray = this.formBuilder.array([]);
-    orderItemsArray.push(
-      this.formBuilder.group({
-        id: [""], //Sequence-based
-        ordersItemsId: [""],
-        item: [""],
-        quantity: [""],
-        price: [""],
-        orderId: [""],
-      })
-    );
-    return orderItemsArray;
-  }
-
-  //Load Customer Data
-  loadCustomerForm(data: any) {
-    console.log("Loading Customer Data...");
-    this.customerForm.patchValue({
-      id: data.id,
-      customerId: data.customerId,
-      firstName: data.firstName,
-      lastName: data.lastName,
-      phone: data.phone,
-      email: data.email,
-      street: data.street,
-      zip: data.zip,
-    });
-
-    //Orders Array
-    console.log("Orders: " + JSON.stringify(data.orders));
-    data.orders.forEach((element) => {
-      console.log("Inside Orders..." + JSON.stringify(element));
-      const order = this.formBuilder.group({
-        id: element.id,
-        orderId: element.orderId,
-        orderStatus: element.orderStatus,
-        orderDate: formatDate(element.orderDate, "yyyy-MM-dd", "en-US"),
-        storeId: element.storeId,
-        customerId: element.customerId,
-      });
-      this.orders.push(order);
-
-      //Order Items Array
-      console.log("Itemz: " + JSON.stringify(element.orderItems));
-      // element.orderItems.forEach((i) => {
-      //   const item = this.formBuilder.group({
-      //     id: i.id,
-      //     orderItemsId: i.orderItemsId,
-      //     item: i.item,
-      //     quantity: i.quantity,
-      //     price: i.price,
-      //     orderId: i.orderId,
-      //   });
-      //   this.orderItems.push(item);
-      // });
-    });
-  }
-
-  //Load Order Data
-  loadOrderForm(data: any) {
-    // this.orderForm.patchValue({
-    //   id: data.id,
-    //   orderId: data.orderId,
-    //   orderStatus: data.orderStatus,
-    //   orderDate: formatDate(data.orderDate, "yyyy-MM-dd", "en-US"),
-    //   storeId: data.storeId,
-    //   customerId: data.customerId,
-    // });
-    //Order Items Array
-    // data.orders.forEach((element) => {
-    //   const items = this.formBuilder.group({
-    //     item: element.item,
-    //     quantity: element.quantity,
-    //     price: element.price,
-    //     orderId: element.orderId,
-    //   });
-    //   this.orders.push(items);
-    // });
-  }
-
-  //Load Items Data
-  loadOrderItemsForm(data: any) {
-    // this.orderItems
   }
 
   get orders() {
     return this.customerForm.get("orders") as FormArray;
   }
 
-  get orderItems() {
-    return this.customerForm.get("orderItems") as FormArray;
-  }
-
-  addOrder() {
+  addOrder(order?: any) {
     console.log("Add Order...");
-    const order = this.formBuilder.group({
-      id: [],
-      orderId: [],
-      orderStatus: [],
-      orderDate: [],
-      storeId: [],
-      customerId: [],
+    const orderr = this.formBuilder.group({
+      id: [order?order.id:''],
+      orderId: [order?order.orderId:''],
+      orderStatus: [order?order.orderStatus:''],
+      orderDate: [order?order.orderDate:''],
+      storeId: [order?order.storeId:''],
+      customerId: [order?order.customerId:''],
+      items: this.formBuilder.array([]),
     });
-    this.orders.push(order);
-  }
+    this.orders.push(orderr);
+
+    let orderIndex = this.orders.length-1;
+    if (!order)
+        this.addItem(orderIndex);
+    else {
+        if (!order.orderItems)  //undefined is true (i.e, no items)
+            console.log("==== Order # "+JSON.stringify(order.id)+" No Items")
+        else 
+            order.orderItems.forEach(item => {
+              this.addItem(orderIndex, item);
+            });
+    }
+ }
 
   deleteOrder(i: any) {
-    console.log("Delete Order...");
     this.orders.removeAt(i);
   }
 
-  addItem() {
-    console.log("Add Item...");
-    const item = this.formBuilder.group({
-      id: [],
-      orderItemsId: [],
-      item: [],
-      quantity: [],
-      price: [],
-      orderId: [],
+  addItem(orderIndex: number, item?: any) {
+    console.log("Add Item..."+JSON.stringify(item));
+    const itemm = this.formBuilder.group({
+      id: [item?item.id:''],
+      orderItemsId: [item?item.orderItemsId:''],
+      item: [item?item.item:''],
+      quantity: [item?item.quantity:''],
+      price: [item?item.price:''],
+      orderId: [item?item.orderId:''],
     });
-    this.orderItems.push(item);
+
+    (<FormArray>(<FormGroup>(<FormArray>this.orders)
+      .controls[orderIndex]).controls['items']).push(itemm)
   }
 
-  deleteItem(x: any) {
-    console.log("Delete Item...");
-    this.orderItems.removeAt(x);
+  delItem(orderIndex: number, index: number) {
+    (<FormArray>(<FormGroup>(<FormArray>this.orders)
+      .controls[orderIndex]).controls['items']).removeAt(index);
   }
+
 }
