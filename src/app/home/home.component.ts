d1f6fce4d0b8d1925fde58ff1d3273cbb41cc7da
Devon Zhen <dz89@nji.edu>
2021-06-18 20:23:55 -0400

    Update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { DataService } from "./../services/data.service";
-import { MatDialog } from "@angular/material/dialog";
 import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
 
 @Component({
@@ -11,77 +10,64 @@ import { FormGroup, FormBuilder, FormArray } from "@angular/forms";
 export class HomeComponent implements OnInit {
 
   customerForm: FormGroup;
-  orderForm: FormGroup;
-
-  customers: any[] = [];
 
   constructor(private api: DataService,
-              public dialog: MatDialog,
               private formBuilder: FormBuilder) 
   {
-    this.buildCustomerForm();
+    this.customerForm = this.formBuilder.group({
+      'customers': formBuilder.array([]),
+    })
   }
 
   ngOnInit(): void {
 
-    this.customerData();
+    this.initCustomers();
 
   }
 
-  onSubmit() {
-    console.log("Hello");
-    //Display Reactive Form's JSON Values
-    console.log("Form Data: " + JSON.stringify(this.customerForm.value));
+  get customers() {
+    return this.customerForm.get("customers") as FormArray;
   }
 
-  //Display JSON Data
-  customerData() {
+  initCustomers() : any { 
     this.api.getCustomerData().subscribe((data) => {
-      console.log("FULL CUSTOMER DATA: " + JSON.stringify(data)); 
-      this.customers = data;
-      data.forEach(customer => {
-        console.log("EACH CUSTOMER DATA: " + JSON.stringify(customer));
-        //Customer Info
-        this.customerForm.patchValue({
-          'id': customer.id,
-          'customerId': customer.id,
-          'firstName': customer.firstName,
-          'lastName': customer.lastName,
-          'phone': customer.phone,
-          'email': customer.email,
-          'street': customer.street,
-          'zip': customer.zip,
-        });   
-        //Order Info
+      //console.log("FULL CUSTOMER DATA: " + JSON.stringify(data)); 
+      if (data) {
+          data.forEach(customer => {
+            this.addCustomer(customer);
+          });         
+      } else {
+          this.addCustomer();
+      }
+    });
+  }
+
+  addCustomer(customer?: any)
+  {
+    const cust = this.formBuilder.group({
+      id: [customer?customer.id:''],
+      customerId: [customer?customer.customerId:''],
+      firstName: [customer?customer.firstName:''],
+      lastName: [customer?customer.lastName:''],
+      phone: [customer?customer.phone:''],
+      email: [customer?customer.email:''],
+      street: [customer?customer.street:''],
+      zip: [customer?customer.zip:''],
+      orders: this.formBuilder.array([]), 
+    });    
+    this.customers.push(cust);
+
+    let custIndex = this.customers.length-1
+    if (!customer) {
+        this.addOrder(custIndex);
+    } else {
         customer.orders.forEach(order => {
-          this.addOrder(order);
+          this.addOrder(custIndex, order);
         });
-
-      });
-    });
+    }
   }
 
-  //Build Customer Form
-  buildCustomerForm() {
-    this.customerForm = this.formBuilder.group({
-      id: [""],
-      customerId: [""],
-      firstName: [""],
-      lastName: [""],
-      phone: [""],
-      email: [""],
-      street: [""],
-      zip: [""],
-      //Order Array
-      orders: this.formBuilder.array([]), //this.buildOrderForm(),
-    });
-  }
-
-  get orders() {
-    return this.customerForm.get("orders") as FormArray;
-  }
-
-  addOrder(order?: any) 
+  addOrder(custIndex: number, order?: any) 
   {
     const orderr = this.formBuilder.group({
       id: [order?order.id:''],
@@ -92,26 +78,26 @@ export class HomeComponent implements OnInit {
       customerId: [order?order.customerId:''],
       items: this.formBuilder.array([]),
     });
-    this.orders.push(orderr);
-
-    let orderIndex = this.orders.length-1;
-    if (!order)                 //undefined is true (i.e, no order)
-        this.addItem(orderIndex);
-    else {
-        if (!order.orderItems)  //undefined is true (i.e, no item)
-            this.addItem(orderIndex);          
-        else 
-            order.orderItems.forEach(item => {
-              this.addItem(orderIndex, item);
+    (<FormArray>(<FormGroup>(<FormArray>this.customers)
+          .controls[custIndex]).controls['orders']).push(orderr);
+          
+    let orderIndex = (<FormArray>(<FormGroup>(<FormArray>this.customers)
+                         .controls[custIndex]).controls['orders']).length-1   
+  
+    if (!order) {                 //undefined is true (i.e, no order) 
+        this.addItem(custIndex, orderIndex);
+    } else {
+        if (!order.orderItems) {  //undefined is true (i.e, no item) 
+            this.addItem(custIndex, orderIndex);
+        } else {
+            order.orderItems.forEach(order => {
+              this.addItem(custIndex, orderIndex, order);
             });
+        }
     }
   }
-
-  deleteOrder(i: any) {
-    this.orders.removeAt(i);
-  }
-
-  addItem(orderIndex: number, item?: any) {
+  
+  addItem(custIndex: number, orderIndex: number, item?: any) {
     const itemm = this.formBuilder.group({
       id: [item?item.id:''],
       orderItemsId: [item?item.orderItemsId:''],
@@ -121,13 +107,29 @@ export class HomeComponent implements OnInit {
       orderId: [item?item.orderId:''],
     });
 
-    (<FormArray>(<FormGroup>(<FormArray>this.orders)
-      .controls[orderIndex]).controls['items']).push(itemm)
+    (<FormArray>(<FormGroup>(<FormArray>
+        (<FormArray>(<FormGroup>(<FormArray>this.customers)    // this orders
+                     .controls[custIndex]).controls['orders']) )
+        .controls[orderIndex]).controls['items']).push(itemm);
   }
 
-  delItem(orderIndex: number, index: number) {
-    (<FormArray>(<FormGroup>(<FormArray>this.orders)
-      .controls[orderIndex]).controls['items']).removeAt(index);
+  delOrder(custIndex: number, orderIndex: number) {
+    (<FormArray>(<FormGroup>(<FormArray>this.customers)
+                     .controls[custIndex]).controls['orders']).removeAt(orderIndex);
   }
+
+  delItem(custIndex: number, orderIndex: number, itemIndex: number) {
+    (<FormArray>(<FormGroup>(<FormArray>
+        (<FormArray>(<FormGroup>(<FormArray>this.customers)    
+                     .controls[custIndex]).controls['orders']) )
+        .controls[orderIndex]).controls['items']).removeAt(itemIndex);
+  }
+
+
+  onSubmit() {
+    //Display Reactive Form's JSON Values
+    console.log("Form Data: " + JSON.stringify(this.customerForm.value));
+  }
+
 
 }
